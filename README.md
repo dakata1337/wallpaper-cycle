83aa4f8131b4bebdbe032ba5fa5702c543669562
dakata1337 <poshtazaprezentaciaa@gmail.com>
2022-06-22 22:21:11 +0300

    Added source code & README

---
+++ README.md
@@ -0,0 +1,20 @@
+# wallpaper-cycle
+A simple program that cycles through wallpapers. Supporting only X11 using feh.
+
+## Dependencies
+- python
+- feh
+
+## Usage
+### On startup (~/.xinitrc)
+```sh
+python /path/to/cycle.py -t 300 -p ~/Wallpapers &
+exec your_wm
+```
+
+### Options
+```
+-h    shows this help message
+-p    the directory in which the wallpapers are located
+-t    the time between each change (in seconds)
+```
---
+++ cycle.py
@@ -0,0 +1,53 @@
+import sys, getopt
+import subprocess
+import random
+import time
+import os
+
+__relfile__=os.path.basename(__file__)
+
+def change_wallpaper(wp_path: str):
+    subprocess.run(["/usr/bin/feh", "--bg-scale", wp_path])
+
+def start(path: str, timeout: int):
+    while True:
+        files = os.listdir(path)
+        random.shuffle(files)
+        for _ in range(len(files)):
+            file = files.pop(random.randrange(len(files)))
+            change_wallpaper(f"{path}/{file}")
+            time.sleep(timeout)
+
+def bold(string: str) -> str:
+    return f"\x1b[1m{string}\x1b[0m"
+
+def usage():
+    print(f"""\
+{__relfile__} version 0.1
+Usage: {__relfile__} -t [TIMEOUT] -p [PATH]
+  {bold('-h')}\t shows this help message
+  {bold('-p')}\t the directory in which the wallpapers are located
+  {bold('-t')}\t the time between each change""")
+    sys.exit(0)
+
+def main():
+    opts, _ = getopt.getopt(sys.argv[1:], "hp:t:")
+
+    path: str = ""
+    timeout: int = 300
+    for arg, val in opts:
+        if arg == "-h":
+            usage()
+        elif arg == "-p":
+            path = val
+        elif arg == "-t":
+            timeout = int(val)
+
+    if len(path) == 0:
+        print("Path option is required! Use -h for more info.")
+        sys.exit(1)
+    start(path, timeout)
+
+if __name__ == "__main__":
+    main()
+
